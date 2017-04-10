4e4e547606f71457a94de2b0697d58fd0ed14189
kellykristl <goddesswebdesign@gmail.com>
2017-04-10 14:10:10 -0600

    modified about emily

--- index.php
+++ index.php
@@ -14,6 +14,6 @@
 		<h2 ALIGN="center">Hey! My name is Commander Troi.</h2>
 		<p align="center"><img src="images/troi.jpg" alt="Oops" /></p>
 		<p ALIGN="CENTER">I am married to Riker and enjoy star gazing.</p>
-		<p ALIGN="CENTER">I have travelled back in time and joined this bootcamp in order to attempt to change the future for my unborn child.</p>
+		<p ALIGN="CENTER">I have traveled back in time and joined this bootcamp in order to attempt to change the future for my unborn child.</p>
 	</body>
 </html>
