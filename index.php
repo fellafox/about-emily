f918263986d65adc7b514bbc1c12347783a84b69
Emily Halleran <emily@blueroserevival.com>
2017-04-10 11:07:40 -0600

    Draft 1

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>About Me</title>
+	</head>
+	<body>
+		<img src="C:\Users\Emily Rose\Google Drive\Bootcamp\git\about-emily\images\troi.jpg"/>
+		<h1>Hey! My name is Commander Troi.</h1><p>I am married to Riker and enjoy star gazing.</p>
+		<P>I have travelled back in time and joined this bootcamp in order to attemp to change the future for my unborn child</P>
+	</body>
+</html>
