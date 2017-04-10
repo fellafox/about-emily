17c8f5b2cee4f6bea985561598f5eba9fd676055
Emily Halleran <emily@blueroserevival.com>
2017-04-10 11:43:17 -0600

    Draft 1

--- index.php
+++ index.php
@@ -3,10 +3,16 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>About Me</title>
+		<style>
+			body {
+				background-color: aqua;
+				color: darkslategrey;
+			}
+		</style>
 	</head>
 	<body>
-		<img src="images/troi.jpg" alt="Oops"/>
-		<h1>Hey! My name is Commander Troi.</h1><p>I am married to Riker and enjoy star gazing.</p>
-		<h2>I have travelled back in time and joined this bootcamp in order to attemp to change the future for my unborn child</h2>
+		<img src="images/troi.jpg" alt="Oops" />
+		<h2 ALIGN="center">Hey! My name is Commander Troi.</h2><p ALIGN="CENTER">I am married to Riker and enjoy star gazing.</p>
+		<p ALIGN="CENTER">I have travelled back in time and joined this bootcamp in order to attemp to change the future for my unborn child.</p>
 	</body>
 </html>
